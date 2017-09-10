b2bd0ddbebc1cd499d9bf1e2fbda0a6c8bd28148
Goular <zhaojt_cs@126.com>
2017-09-10 23:59:39 +0800

    提交数据库的监听

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,16 @@ class AppServiceProvider extends ServiceProvider
     {
         //如果你正在运行的 MySQL release 版本低于5.7.7 或 MariaDB release 版本低于10.2.2 ，为了MySQL为它们创建索引，你可能需要手动配置迁移生成的默认字符串长度
         Schema::defaultStringLength(191);
+
+        //用于数据库的监听
+        \DB::listen(function ($query) {
+            $sql = $query->sql;
+            //PDO预处理需要绑定的对象
+            $binding = $query->bindings;
+            $time = $query->time;
+            //var_export -- 输出或返回一个变量的字符串表示
+            \Log::debug('SQL查询详情',compact('time','sql', 'binding'));
+        });
     }
 
     /**
