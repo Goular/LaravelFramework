3eebb9b24c6c09a9da959769aab85ceae1f5c5a2
Goular <zhaojt_cs@126.com>
2017-09-12 11:58:52 +0800

    提交内容

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /**
+     * 注册需要展示的页面
+     */
+    public function index(){
+        return "123";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,23 @@
 //前台模块
 Route::get('/','\App\Http\Controllers\IndexController@index');
 
-//验证码模块
-//Route::any('/','\App\Http\Controllers\CaptchaController@createImg');
-
 //登录模块
 Route::get('/login','\App\Http\Controllers\LoginController@index');
+
+//注册模块
+Route::get('/register','\App\Http\Controllers\RegisterController@index');
+
+//验证模块
+Route::group(['prefix' => 'validate'], function () {
+    //验证码模块的使用
+    Route::get('/captcha/img','\App\Http\Controllers\CaptchaController@createImg');
+    Route::get('/captcha/imgUrl','\App\Http\Controllers\CaptchaController@createUrl');
+    Route::get('/captcha/imgHtml','\App\Http\Controllers\CaptchaController@createHtml');
+
+    //验证短信发送模块的使用
+    Route::get('/sms/code/{id}','\App\Http\Controllers\SmsController@sendSms');
+});
+
+
+
+
